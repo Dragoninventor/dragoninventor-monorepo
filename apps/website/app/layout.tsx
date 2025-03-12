4742e2207613b574241ed1d0977dce0af8e056cb
michael <michael@dragoninventor.com>
2025-03-12 10:45:53 -0600

    Fixed layout style inconsistency on page padding

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 		<>
 			<html lang="en-us">
 			<body
-				className={`${inter.className} ${default_card_font.variable} ${default_cardpage_font.variable} relative grid min-h-screen grid-rows-1 flex-col justify-items-center px-4 py-2.5`} // bg-indigo-200
+				className={`${inter.className} ${default_card_font.variable} ${default_cardpage_font.variable} relative grid min-h-screen grid-rows-1 flex-col justify-items-center px-4 py-2.5 sm:py-4`} // bg-indigo-200
 				style={{
 					background:
 						"linear-gradient(160deg, rgba(87,106,168,1) 0%, rgba(115,132,196,1) 10%, rgba(152,164,219,1) 40%, rgba(197,189,247,1) 75%, rgba(204,163,231,1) 100%)",
