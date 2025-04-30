2566537db3856d1387cce4acf8ac88a646a68687
michael <michael@dragoninventor.com>
2025-04-30 14:33:47 -0600

    Updated 404 page for the main website

--- apps/website/app/not-found.tsx
+++ apps/website/app/not-found.tsx
@@ -1,9 +1,16 @@
+import { CardContent } from "@dragoninventor/ui";
+import Link from "next/link";
+
 const NotFound = () => {
 	return (
-		<div>
-			<h2>Page Not Found</h2>
-			<p>lol</p>
-		</div>
+		<CardContent>
+			<h1 className={"text-center"}>404 — Page Not Found</h1>
+			<h2 className={"text-center italic mb-4 text-gray-600"}>Here be dragons.</h2>
+			<p>Sorry! The page you are looking for does not exist.</p>
+			<p>Would you like to return <Link href={"/"}
+											  className={"text-indigo-500 underline transition-colors hover:text-indigo-700"}>home</Link>?
+			</p>
+		</CardContent>
 	);
 };
 
